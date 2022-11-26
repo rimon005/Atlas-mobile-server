b241c8a5bcb136fae7bc33faeb373d5203fc96d1
Asif Iqbal Rimon <ariyan8185@gmail.com>
2022-11-26 23:59:13 +0600

    make admin

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 require('dotenv').config()
 
@@ -64,10 +64,59 @@ const run = async () => {
             res.send(result)
         })
 
+        app.get('/users/buyer' , async(req , res) => {
+            let query = {}
+            if(req.query.role){
+                query = {
+                    role : req.query.role
+                }
+            }
+            console.log(query);
+            const users = await usersCollection.find(query).toArray();
+            res.send(users)
+        })
+
+        app.put('/users/admin/:id' , async(req , res) => {
+            const id = req.params.id;
+            const query = {_id:ObjectId(id)};
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set : {
+                    role : 'admin'
+                }
+            }
+            const users = await usersCollection.updateOne(query , updatedDoc , options);
+            res.send(users)
+        })
+
+        app.get('/users/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email };
+            const user = await usersCollection.findOne(query);
+            res.send({ isAdmin: user?.role === 'admin' })
+        })
+
+        app.get('/users' , async(req , res ) => {
+            const query = {};
+            const users = await usersCollection.find(query).toArray();
+            res.send(users)
+        })
+
         app.post('/bookings' , async(req , res ) => {
             const booking = req.body;
             const result = await bookingsCollection.insertOne(booking);
             res.send(result)
+        })
+
+        app.get('/bookings' , verifyJWT,  async(req , res) => {
+            const email = req.query.email;
+            const decodedEmail = req.decoded.email
+            if (email !== decodedEmail) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+            const query = {email : email};
+            const bookings = await bookingsCollection.find(query).toArray();
+            res.send(bookings)
         })
         // Jwt toke 
         app.get('/jwt', async (req, res) => {
